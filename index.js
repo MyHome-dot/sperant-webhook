a130d912d655aa53e3f404c50f05472f97fa236d
MyHome-dot <trafficker.nativoslab@gmail.com>
2026-05-11 11:56:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const SPERANT_TOKEN = '5PrYiXZgyc56UCSZNoVbeDYjCRX6diO4dlyWOs05';
+const SPERANT_TOKEN = '4qTeCts5Yr98FnagiRDCt5F5eTsQApk36p4dnMG3';
 const SPERANT_URL = 'https://api.eterniasoft.com/v3/clients';
 
 app.post('/webhook-ghl', async (req, res) => {
